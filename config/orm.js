2b64bc6220906aad3f00d40524a9fb5d9521466b
Marc <mlanglois333@icloud.com>
2020-09-16 11:07:51 -0400

    Created ORM

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,42 @@
 var connection = require("../config/connection.js");
 
-function selectAll(){}
-function insertOne(){}
-function updateOne(){}
-function deleteOne(){}
+var orm = {
+
+    all: function(table, burg){
+        var qu= "SELECT * FROM " + table + ";";
+        connection.query(qu, function(err, result){
+            if (err) throw err;
+            burg(result);
+        });
+    },
+
+    create: function(table, cols, vals, burg){
+        var colst = cols.toString();
+        var qu = "INSERT INTO " + table + "(" + colst + ") VALUES (" + vals + ")";
+
+        connection.query(qu, function(err, result) {
+            if (err) throw err;
+            burg(result);
+        });
+    },
+
+    update: function(table, vals, condition, burg){
+        var qu = "UPDATE " + table + " SET " + vals + " WHERE " + condition;
+        connection.query(qu, function(err, result){
+            if (err) throw err;
+            burg(result);
+        });
+    },
+
+    delete: function(table, condition, burg){
+        var qu="DELETE FROM " + table + " WHERE " + condition;
+
+        connection.query(qu, function(err, result){
+            if (err) throw err;
+            burg(result);
+        });
+    }
+};
 
 
 module.exports = orm;
