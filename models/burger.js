3163c198cc1ca854542039f9d12c8cde76b20181
Marc <mlanglois333@icloud.com>
2020-09-16 10:31:20 -0400

    Model Created

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,29 @@
 const orm = require("../config/orm.js");
 
+var burger= {
+    all: function(burg){
+        orm.all("burgers", function(res){
+            burg(res);
+        });
+    },
 
+    create: function(cols, vals, burg) {
+        orm.create("burgers",cols, vals, function(res){
+            burg(res);
+        });
+    },
+
+    update: function(objColVals, condition, burg) {
+        orm.updte("burgers", objColVals, condition, function(res) {
+            burg(res);
+        });
+    },
+    delete: function(condition, burg) {
+        orm.delete("burgers", condition, function(res){
+            burg(res);
+        });
+    }
+};
 
 
 module.exports = burger
